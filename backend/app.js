7a91eb4c659f770d66bc7462ecb877380d947f42
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-01-29 15:22:56 +0200

    Added CORS

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const journeys = require("./routes/journeys");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use("/api/journeys", journeys);
 
